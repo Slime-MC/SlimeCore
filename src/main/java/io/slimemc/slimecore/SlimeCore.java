f74af5ee9173dbea48b7ea05cffe5fd876b929d0
Yannick <48063311+CaptainYannick@users.noreply.github.com>
2021-10-17 16:00:06 +0200

    Version

--- src/main/java/io/slimemc/slimecore/SlimeCore.java
+++ src/main/java/io/slimemc/slimecore/SlimeCore.java
@@ -6,8 +6,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public abstract class SlimeCore extends JavaPlugin {
 
-    private final String coreVersion = "1.0.0-alpha.1";
-    private final int coreVersionAsInt = 1;
+    private final String coreVersion = "1.0.0-alpha.2";
+    private final int coreVersionAsInt = 2;
 
     private boolean error = false;
 
